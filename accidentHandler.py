bafb92e8c4e37f3f4c461b6b92b6bd5345ce91c8
Louis Fernandes <fern.louis@gmail.com>
2016-06-02 17:40:48 -0400

    Including the test data set.

---
+++ accidentHandler.py
@@ -0,0 +1,18 @@
+"""
+accidentHandler.py
+This function is my initial attempt at parsing and visualizing the accident
+data present in the Mass DOT RMV data sets.
+Current features:
+
+Works in progress:
+
+Contents:
+
+Acknowledgements:
+The Insight Health Data Science program, for giving me the opportunity to work
+in an entirely new field.
+
+My girlfriend, for putting up with me.
+
+Created by Louis Fernandes, 2016 06 02.
+"""
