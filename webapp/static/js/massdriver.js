49c8e4f0eebaf98ce831478f1689a171a77df3f2
Louis Fernandes <fern.louis@gmail.com>
2016-06-14 23:31:41 -0400

    Good progress on converting over to the Google Maps API. I have standard direction finding taken care of; next, I need to pass the latitude and longitude to my Python to get a least path.

--- webapp/static/js/massdriver.js
+++ webapp/static/js/massdriver.js
@@ -6,13 +6,43 @@ var paths = [];
 var markers = [];
 var accessToken = '';
 var message = '';
+var map
+var flightPath
+var isline = 0
+var directionsDisplay
+var directionsService
+var geocoder
 
-function Load() {
-    // Initialize MapBox map<script>
-    L.mapbox.accessToken = accessToken;
-    map = L.mapbox.map('map', '').setView([42.373, -71.103], 13);
-};
+function initMap() {
+	directionsDisplay = new google.maps.DirectionsRenderer;
+  	directionsService = new google.maps.DirectionsService;
+  	geocoder = new google.maps.Geocoder();
+	var mapDiv = document.getElementById('map');
+	map = new google.maps.Map(mapDiv)
+	var southWest = new google.maps.LatLng(41.65, -73.5);
+	var northEast = new google.maps.LatLng(42.88, -69.95);
+	var bounds = new google.maps.LatLngBounds(southWest,northEast);
+	map.fitBounds(bounds);
+	
+	directionsDisplay.setMap(map);
 
+	// calculateAndDisplayRoute('Springfield, MA', 'Boston, MA', directionsService, directionsDisplay);
+
+}
+
+function calculateAndDisplayRoute(orig, dest, directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: orig, 
+    destination: dest,
+    travelMode: google.maps.TravelMode.DRIVING
+  }, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
 
 function ShowLoading(showLoading) {
     if (showLoading) {
@@ -20,38 +50,6 @@ function ShowLoading(showLoading) {
     } else {
         $('#loading').css('visibility', 'hidden');
     };
-};
-
-
-// The Following Section Finds Your Route
-function FindAndRoute(startPt, endPt, runDis){
-   ShowLoading(true);
-   message = '';
-   $('#addmessage').html(message);
-   $.getJSON('/findRoute', {'s': startPt,'e': endPt,'d':runDis}, function(findJSON){
-          // Maybe reset on bad query
-          ShowLoading(false);
-          //console.log(findJSON);
-          //console.log(jQuery.isEmptyObject(findJSON));
-          if (!jQuery.isEmptyObject(findJSON)) {
-          //if (findJSON!={}) {
-             console.log('Path Found');
-             message = findJSON['message'];
-             $('#addmessage').html(message);
-
-             // Add geoMarkers to feature llayer
-             geoJSON.push(findJSON['path']);
-             map.featureLayer.setGeoJSON(geoJSON);
-
-             //Pan and zoom
-             map.fitBounds(findJSON['bounds']);
-          }else{
-             //console.log('Path NOT Found');
-             message = 'Path not found! Please check your address, and make sure they are within the blue boundary of Cambridge and Boston.';
-             //console.log(message);
-             $('#addmessage').html(message);
-          }
-   });
 };
 
 function PathSearch() {
@@ -77,36 +75,45 @@ function PathSearch() {
 
 };
 
+function getDirections(map){
+	var fromdest = document.getElementById('startPt').value;
+	var todest = document.getElementById('endPt').value;
+	calculateAndDisplayRoute(fromdest, todest, directionsService, directionsDisplay);
+	geocodePoints(fromdest);
+	geocodePoints(todest);
+}
 
-function traverse(){
+function geocodePoints(address) {
+	geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        alert("Geocoding successful!")
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+   });
+
+}
+
+function traverse(map){
 // This function takes in locations and returns geojson path
-    var path = [[-73.5, 42.88],
-			   [-72.0, 41.70],
-			   [-70.0, 41.86],
-			   [-71.3, 41.55]]
-    map.addSource("route", {
-		"type": "geojson",
-		"data": {
-				"type": "Feature",
-				"properties": {},
-				"geometry": {
-					"type": "LineString",
-					"coordinates": path
-				}
+	if (isline==1){
+		flightPath.setMap(null)
 		}
-	});
-	map.addLayer({
-		"id": "route",
-		"type": "line",
-		"source": "route",
-		"layout": {
-				"line-join": "round",
-				"line-cap": "round"
-		},
-		"paint": {
-				"line-color": "#888",
-				"line-width": 8
-		}
-	});
+  var pathcoords = [
+    {lat: 42.88, lng: -73.5+Math.random()},
+    {lat: 41.7, lng: -72},
+    {lat: 41.86, lng: -70},
+    {lat: 41.55, lng: -71.3}
+  ];
+  flightPath = new google.maps.Polyline({
+    path: pathcoords,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
+	
+  flightPath.setMap(map);
+	isline=1
 
 };
